1eadb4b65b7fada96b6c1d1e85155d8eec23381a
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-20 07:22:59 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # HTTP URLs that Could Not Be Fixed
    These URLs were unable to be fixed. Please review them to see if they can be manually resolved.
    
    * http://my-website.com/path/to/image.jpg (200) with 1 occurrences could not be migrated:
       ([https](https://my-website.com/path/to/image.jpg) result ConnectTimeoutException).
    
    # Fixed URLs
    
    ## Fixed But Review Recommended
    These URLs were fixed, but the https status was not OK. However, the https status was the same as the http request or http redirected to an https URL, so they were migrated. Your review is recommended.
    
    * http://flickr.com/icons/bobby.jpg (403) with 1 occurrences migrated to:
      https://flickr.com/icons/bobby.jpg ([https](https://flickr.com/icons/bobby.jpg) result 403).
    * http://example.com/path/to/thumb.png (404) with 1 occurrences migrated to:
      https://example.com/path/to/thumb.png ([https](https://example.com/path/to/thumb.png) result 404).
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * http://projectreactor.io with 4 occurrences migrated to:
      https://projectreactor.io ([https](https://projectreactor.io) result 200).
    * http://flickr.com/bobby/ with 1 occurrences migrated to:
      https://flickr.com/bobby/ ([https](https://flickr.com/bobby/) result 302).

--- src/main/kotlin/io/projectreactor/bot/slack/data/Attachment.kt
+++ src/main/kotlin/io/projectreactor/bot/slack/data/Attachment.kt
@@ -7,11 +7,11 @@ data class Attachment(
         val color: String? = null, //eg. "#36a64f"
         val pretext: String? = null, //Optional text that appears above the attachment block
         val author_name: String? = null, //eg. "Bobby Tables"
-        val author_link: String? = null, //eg. "http://flickr.com/bobby/"
-        val author_icon: String? = null, //eg. "http://flickr.com/icons/bobby.jpg"
+        val author_link: String? = null, //eg. "https://flickr.com/bobby/"
+        val author_icon: String? = null, //eg. "https://flickr.com/icons/bobby.jpg"
         val text: String? = null, //Optional text that appears within the attachment
         val image_url: String? = null, //eg "http://my-website.com/path/to/image.jpg"
-        val thumb_url: String? = null, //eg. "http://example.com/path/to/thumb.png"
+        val thumb_url: String? = null, //eg. "https://example.com/path/to/thumb.png"
         val footer: String? = null, //eg. "Slack API"
         val footer_icon: String? = null, //eg. "https://platform.slack-edge.com/img/default_application_icon.png"
         val ts: Int? = null, //timestamp to reference if message relates to specific instant
